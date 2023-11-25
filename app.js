bb90c49158cc7eab9cd977e334cdfaf98c8c7347
Kamil Gulzar <kamilgulzar289@gmail.com>
2023-11-25 18:21:22 +0500

    create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+// JS Assignment 3
+
+
+// Q1
+// var student_name = [];
+
+
+// Q2
+// var student_name = {
+//    stdname: []   
+// }
+
+// Q3
+// var countries = ["PAK","IND","UAE","USA","UK"];
+
+
+// Q4
+// var marks = [97,98,92,86];
+
+// Q5
+// var tOrF = [true,false];
+
+// Q6
+// var biodata = ["Kamil",10,"Bilal",17,"Murtaza",11,true];
+
+// Q7
+// var qualification = ["Qualification:"+"</br>"+"1) SSC"+"</br>"+"2) HSC"+"</br>"+"3) BCS"+"</br>"+"4) BS"+"</br>"+"5) BCOM" + "</br>"+ "6) MS" + "</br>" + "7) M.Phil."+"</br>"+"8) PhD"]
+// document.write(qualification)
+
+// Q8
+// var names = ["Michael","John","Tony"];
+// var marks = [425,328,487];
+// document.write("Score of "+names[0]+" is"+ " "+marks[0]+"."+" Percentage: "+(marks[0]/500)*100+"%" + "</br>")
+// document.write("Score of "+names[1]+" is"+ " "+marks[1]+"."+" Percentage: "+(marks[1]/500)*100+"%"+"</br>")
+// document.write("Score of "+names[2]+" is"+ " "+marks[2]+"."+" Percentage: "+(marks[2]/500)*100+"%")
+
